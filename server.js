801826ab422028515ec4a91bea3c879a9e7ba97a
Rafael Brandão <r.augusto.brandao@gmail.com>
2018-06-05 22:29:39 -0300

    Changed name from teste to example

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 module.exports = function(modules){
 
-	//Teste
-	var teste = {};
-	teste.controllers = {};
-	teste.controllers.teste = require(__dirname + '/modules/teste/teste-controller.js')();
+	//Example
+	var example = {};
+	example.controllers = {};
+	example.controllers.example = require(__dirname + '/modules/example/example-controller.js')();
 
 	//Rotas
 	var routes = {};
 	routes.routes = require(__dirname + '/routes/router.js')(modules.express, routes);
 	routes.v1 = {};
-	routes.v1.teste = require(__dirname + '/routes/v1/teste.js')(teste);
+	routes.v1.example = require(__dirname + '/routes/v1/example.js')(example);
 
 	return routes.routes;
 
